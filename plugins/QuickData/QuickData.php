5726f2337de73f1cbc7e4f5f9aa75a85fbd51e81
Oleg Malii <oleg@seotoaster.com>
2014-09-25 13:55:40 +0300

    Add quickData plugin

---
+++ plugins/QuickData/QuickData.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Piwik\Plugins\QuickData;
+
+class MyPlugin extends \Piwik\Plugin {
+
+    public function getInformation() {
+        return array(
+            'description' => 'Quick data access',
+            'author' => 'Oleg Malii',
+            'author_homepage' => 'http://seosamba.com/',
+            'homepage' => 'http://seosamba.com/',
+            'license' => 'GPL v3 or later',
+            'license_homepage' => 'http://www.gnu.org/licenses/gpl.html',
+            'version' => '1.0',
+            'translationAvailable' => false
+        );
+    }
+
+}
